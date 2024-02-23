1b12383ef0ac49030bb7f89c9009778198b24a50
BOUNTY707 <mehrobakooo@gmail.com>
2024-02-23 21:16:24 +0500

    40% tugadi

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let charterSlide = new Swiper(".charterSlide", {
+    slidesPerView: 3,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var yachtSlide = new Swiper(".yachtSlide", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    
+});
